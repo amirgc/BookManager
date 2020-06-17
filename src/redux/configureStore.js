ed1b28679961638b4a7e2d1b295b624450d04af2
amirgc <gcamir15@gmail.com>
2020-06-16 23:44:48 -0400

    some issue fixed

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
-export default function configureStore(initialSate) {
-  const composeEnhancer =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for redux dev tools
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+
   return createStore(
     rootReducer,
-    initialSate,
-    composeEnhancer(applyMiddleware(reduxImmutableStateInvariant()))
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
 }
